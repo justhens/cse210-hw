fb573f0cd4293278f8676911bfd4c6f809b1e8e7
justhens <justhens@gmail.com>
2026-01-06 15:55:18 -0500

    C# Programming Exercise 5: Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        ShowWelcome();
+        string name = GetUserName();
+        int favoriteNumber = GetFavoriteNumber();
+        int squared = Square(favoriteNumber);
+        ShowResult(name, squared);
+    }
+    static void ShowWelcome()
+    {
+        Console.WriteLine("Hello! Welcome to this program!");
+    }
+    static string GetUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+    static int GetFavoriteNumber()
+    {
+        int number;
+        bool isValid;
+        do
+        {
+            Console.Write("Enter your favorite number: ");
+            string input = Console.ReadLine();
+            isValid = int.TryParse(input, out number);
+            if (!isValid)
+            {
+                Console.WriteLine("Oops! That is not a valid number. Please try again.");
+            }
+        } while (!isValid);
+        return number;
+    }
+    static int Square(int num)
+    {
+        return num * num;
+    }
+    static void ShowResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
