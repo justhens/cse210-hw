93ef5129c4ecafc7b1157049faafceedce2d4086
justhens <justhens@gmail.com>
2026-01-06 15:10:04 -0500

    C# Programming Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> values = new List<int>();
+        Console.WriteLine("Enter a list of numbers one at a time, enter 0 when finished.");
+        int input;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                values.Add(input);
+            }
+
+        } while (input != 0);
+
+        int total = 0;
+        int largest = values[0];
+
+        foreach (int value in values)
+        {
+            total += value;
+
+            if (value > largest)
+            {
+                largest = value;
+            }
+        }
+
+        double average = (double)total / values.Count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
